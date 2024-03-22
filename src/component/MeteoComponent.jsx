8d570828e6c618aaae4db9bef2dd8460de1f8b22
marco9727 <marco.albarella97@gmail.com>
2024-03-22 17:27:11 +0100

    commit aggiunta spinner e altri dettagli

--- src/component/MeteoComponent.jsx
+++ src/component/MeteoComponent.jsx
@@ -6,19 +6,25 @@ import cloudImg from "../assets/cloud.png";
 import drizzleImg from "../assets/drizzle.png";
 import rainImg from "../assets/rain.png";
 import snowImg from "../assets/snow.png";
+import Loading from "./Loading";
 
 const WeatherApp = () => {
-  const [searchCity, setSearchCity] = useState("");
+  const [searchCity, setSearchCity] = useState("naples");
   const [weather, setWeather] = useState(null);
   const [weatherImg, setWeatherImg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const api_Key = "e831c1107f7bec1ac0d966dff99c6676";
 
-  //   useEffect(() => {
-  //     fetchWeatherData("rome");
-  //   }, []);
-
   const fetchWeatherData = () => {
+    setIsLoading(true);
+    setError(null);
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${api_Key}`
     )
@@ -27,16 +33,19 @@ const WeatherApp = () => {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error("Errore: città non trovata");
+          throw new Error(" città non trovata");
         }
       })
       .then((data) => {
         setWeather(data);
         setSearchCity("");
         setWeatherImg(getWeatherImg(data.weather[0].icon));
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log("ERRORE", error);
+        setIsLoading(false);
+        setError(error.message);
       });
   };
 
@@ -90,61 +99,71 @@ const WeatherApp = () => {
             type="button"
             className="btn btn-primary "
           >
-            <BiSearch />
+            <BiSearch className="icon-search" />
           </button>
         </div>
       </div>
-      {weather && (
+      {isLoading ? (
+        <Loading />
+      ) : (
         <>
-          <div>
-            <img
-              src={weatherImg}
-              alt="icon-weather"
-              style={{ width: "200px" }}
-              className="weather-img"
-            />
-          </div>
-          <div className="temp-weather">
-            {weather.main.temp ? kelvinToCelsius(weather.main.temp) : ""}
-            °C
-          </div>
-          <div className="temp-min-max">
-            <div className="text">
-              MIN:{" "}
-              {weather.main.temp_min
-                ? kelvinToCelsius(weather.main.temp_min)
-                : ""}
-              °C
-            </div>
-            <div className="text">
-              MAX:{" "}
-              {weather.main.temp_max
-                ? kelvinToCelsius(weather.main.temp_max)
-                : ""}
-              °C
-            </div>
-          </div>
-          <div className="city">{weather.name}</div>
-          <div className="data-container">
-            <div className="element">
-              <div className="icon">
-                <BiDroplet />
-              </div>
-              <div className="data">
-                <div>{weather.main.humidity} %</div>
-                <div className="text">umidity</div>
-              </div>
-            </div>
-            <div className="element">
-              <div className="icon">
-                <BiWind />
-              </div>
-              <div className="data">
-                <div>{weather.wind.speed} km/h</div>
-                <div className="text">wind</div>
-              </div>
-            </div>
-          </div>
+          {error ? (
+            <div className="error-message">{error}</div>
+          ) : (
+            weather && (
+              <>
+                <div>
+                  <img
+                    src={weatherImg}
+                    alt="icon-weather"
+                    style={{ width: "200px" }}
+                    className="weather-img"
+                  />
+                </div>
+                <div className="temp-weather">
+                  {weather.main.temp ? kelvinToCelsius(weather.main.temp) : ""}
+                  °C
+                </div>
+                <div className="temp-min-max">
+                  <div className="text">
+                    MIN:{" "}
+                    {weather.main.temp_min
+                      ? kelvinToCelsius(weather.main.temp_min)
+                      : ""}
+                    °C
+                  </div>
+                  <div className="text">
+                    MAX:{" "}
+                    {weather.main.temp_max
+                      ? kelvinToCelsius(weather.main.temp_max)
+                      : ""}
+                    °C
+                  </div>
+                </div>
+                <div className="city">{weather.name}</div>
+                <div className="data-container">
+                  <div className="element">
+                    <div className="icon">
+                      <BiDroplet />
+                    </div>
+                    <div className="data">
+                      <div>{weather.main.humidity} %</div>
+                      <div className="text">umidity</div>
+                    </div>
+                  </div>
+                  <div className="element">
+                    <div className="icon">
+                      <BiWind />
+                    </div>
+                    <div className="data">
+                      <div>{weather.wind.speed} km/h</div>
+                      <div className="text">wind</div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          )}
         </>
       )}
     </Container>
